259745f4c63daa465e830c4e5680b49f794e1e87
gracethomp <gracetomlinson26@gmail.com>
2024-09-11 15:52:05 +0300

    GCD methods

---
+++ src/main/java/nau/GCD.java
@@ -0,0 +1,61 @@
+package nau;
+
+import java.util.ArrayList;
+
+public class GCD {
+    public int advancedGCD(int x, int y) {
+        int d = 1;
+        while (x % 2 == 0 && y % 2 == 0) {
+            x /= 2;
+            y /= 2;
+            d *= 2;
+        }
+        int u = x, v = y;
+        int A = 1, B = 0, C = 0, D = 1;
+        do {
+            while (u % 2 == 0) {
+                u /= 2;
+                if (A % 2 == 0 && B % 2 == 0) {
+                    A /= 2;
+                    B /= 2;
+                } else {
+                    A = (A + y) / 2;
+                    B = (B - x) / 2;
+                }
+            }
+            while (v % 2 == 0) {
+                v /= 2;
+                if (C % 2 == 0 && D % 2 == 0) {
+                    C /= 2;
+                    D /= 2;
+                } else {
+                    C = (C + y) / 2;
+                    D = (D - x) / 2;
+                }
+            }
+            if (u >= v) {
+                u = u - v;
+                A = A - C;
+                B = B - D;
+            } else {
+                v = v - u;
+                C = C - A;
+                D = D - B;
+            }
+        } while (u != 0);
+        return d * v;
+    }
+
+    public int findGCD(ArrayList<Integer> numbers) {
+        if (numbers.isEmpty())
+            return 0;
+
+        int result = numbers.getFirst();
+        for (int w = 1; w < numbers.size(); w++) {
+            result = advancedGCD(result, numbers.get(w));
+            if (result == 1)
+                break;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/nau/Main.java
@@ -0,0 +1,11 @@
+package nau;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        GCD gcd = new GCD();
+        System.out.println(gcd.advancedGCD(288, 1488));
+        System.out.println(gcd.advancedGCD(2828, 334));
+    }
+}
