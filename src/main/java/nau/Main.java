998852544d599aa81b678fb2b126e5ae1441420c
gracethomp <gracetomlinson26@gmail.com>
2024-09-13 22:21:00 +0300

    tests with data from files

--- src/main/java/nau/Main.java
+++ src/main/java/nau/Main.java
@@ -1,13 +1,50 @@
 package nau;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
+    public static String readNumbers(String fileName) {
+        InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(fileName);
+        if (inputStream != null) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                return reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            return "";
+        }
+    }
+
+    public static void testGCD(int[] numbers) {
         GCD gcd = new GCD();
-        System.out.println(gcd.advancedGCD(288, 2976));
-        System.out.println(gcd.advancedGCD(2828, 334));
-        System.out.println(gcd.findGCD(144, 20736, 248832));
-        System.out.println(gcd.findGCD(144, 20736, 248832, 12));
+
+        long startTime = System.nanoTime();
+        int result = gcd.findGCD(numbers);
+        long endTime = System.nanoTime();
+
+        double duration = (endTime - startTime) / 1_000_000.0;
+
+        System.out.println("Result: " + result + ". Time of execution: " + duration + " ms");
+    }
+
+    public static void main(String[] args) {
+        int[] numbers1000 = Arrays.stream(readNumbers("Numbers_1000.txt").split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[] numbers10000 = Arrays.stream(readNumbers("Numbers_10000.txt").split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[] numbers100000 = Arrays.stream(readNumbers("Numbers_100000.txt").split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        testGCD(numbers1000);
+        testGCD(numbers10000);
+        testGCD(numbers100000);
     }
 }
